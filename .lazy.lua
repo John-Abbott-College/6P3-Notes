ca5d539ce075837c4d3313631b10de10c68d25fa
michaelhaaf <michael.haaf@gmail.com>
2025-01-20 00:35:54 -0500

    refactor: replace _reference_links.md with quarto shortcode variables
    also, a few formatting changes for about/*.md files

---
+++ .lazy.lua
@@ -0,0 +1,27 @@
+return {
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				["python"] = { "ruff" },
+				["markdown"] = { "mdformat" },
+				["quarto"] = { "mdformat" },
+				["*"] = { "codespell" },
+			},
+			formatters = {
+        mdformat = {
+          prepend_args = { "--ignore-missing-references" },
+        },
+				injected = {
+					options = {
+						ignore_errors = false,
+						lang_to_ext = { quarto = "qmd" },
+					},
+				},
+			},
+			default_format_opts = {
+				lsp_format = "fallback",
+			},
+		},
+	},
+}
